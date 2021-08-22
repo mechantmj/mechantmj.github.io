7595f5c3410a2bcdd5a951f5f19a2ae84ecd17da
mechantmj <85431475+mechantmj@users.noreply.github.com>
2021-08-22 09:43:18 +0200

    Add files via upload

---
+++ data/Forcemilitaire_14.js
@@ -0,0 +1,10 @@
+var json_Forcemilitaire_14 = {
+"type": "FeatureCollection",
+"name": "Forcemilitaire_14",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nom": "AKAL", "Type": "Cavalerie", "Effectif": "1", "valeur": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -2.878709626846593, -5.986158716811852 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nom": "AKAL", "Type": "Soldat", "Effectif": "2", "valeur": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -2.569309730548016, -6.040303698664103 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nom": "AKAL", "Type": "Armée", "Effectif": "3", "valeur": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -2.994734587958558, -9.265797617576753 ] } }
+]
+}
